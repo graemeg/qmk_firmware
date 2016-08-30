c1f85c7d8ab8369ac995e6e33d69f043f6e81210
Graeme Geldenhuys <graemeg@gmail.com>
2016-08-31 00:45:32 +0100

    new layout v1.6 - new F-key and Arrows layers added.
    * I got rid of the old Media layer which I never used.
    * Moved the F-keys from layer 1 top row keys to layer two on the left hand
    * New dedicated arrow key layer.
    * introduced 3 macros for often typed text.
    
    I designed layers 2 (f-keys) and 3 (arrows) so I can use those new keys
    in combination with Ctrl, Alt and Shift. The development environment
    I program in uses those key combinations often.

--- keyboards/ergodox/keymaps/gg_dvorak/keymap.c
+++ keyboards/ergodox/keymaps/gg_dvorak/keymap.c
@@ -4,7 +4,13 @@
 
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
-#define MDIA 2 // media keys
+#define FKEY 2 // function keys
+#define ARWS 3 // arrow keys
+
+#define MACRO_TODO 10
+#define MACRO_ISEQUALS 11
+#define MACRO_PARENTHESE 12
+#define MDBL0 13
 
 /*
   Author: Graeme Geldenhuys <graeme@geldenhuys.co.uk>
@@ -23,21 +29,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ╭────────┬──────┬──────┬──────┬──────┬──────┬──────╮           ╭──────┬──────┬──────┬──────┬──────┬──────┬────────╮
- * │  Esc   │  1  !│  2  @│  3  #│  4  $│  5  %│ INS  │           │ DEL  │  6  ^│  7  &│  8  *│  9  (│  0  )│   \   |│
+ * │  Esc   │  1  !│  2  @│  3  #│  4  $│  5  %│  :=  │           │ INS  │  6  ^│  7  &│  8  *│  9  (│  0  )│   \   |│
  * ├────────┼──────┼──────┼──────┼──────┼──────┼──────┤           ├──────┼──────┼──────┼──────┼──────┼──────┼────────┤
  * │  Tab   │  '  "│  ,  <│  .  >│  P   │   Y  │ Del  │           │ BkSp │   F  │   G  │   C  │   R  │   L  │   /   ?│
  * ├────────┼──────┼──────┼──────┼──────┼──────┤      │           │      ├──────┼──────┼──────┼──────┼──────┼────────┤
- * │  =/+   │A /~L2│   O  │   E  │  U • │   I  ├──────┤           ├──────┤   D  │ • H  │   T  │   N  │S /~L2│   ─   _│
+ * │  =/+   │A /~L3│   O  │   E  │  U • │   I  ├──────┤           ├──────┤   D  │ • H  │   T  │   N  │S /~L2│   ─   _│
  * ├────────┼──────┼──────┼──────┼──────┼──────┤ C+F9 │           │ Enter├──────┼──────┼──────┼──────┼──────┼────────┤
  * │ LShift │;/Ctrl│   Q  │   J  │  K   │   X  │      │           │      │   B  │   M  │   W  │   V  │Z/Ctrl│ RShift │
  * ╰─┬──────┼──────┼──────┼──────┼──────┼──────┴──────╯           ╰──────┴──────┼──────┼──────┼──────┼──────┼──────┬─╯
- *   │ Ctrl │  Alt │ CUT  │ COPY │ PASTE│                                       │ Left │  Up  │ Down │ Right│ SLCK │
+ *   │LCtrl │ LAlt │ CUT  │ COPY │ PASTE│                                       │ Left │  Up  │ Down │ Right│ SLock│
  *   ╰──────┴──────┴──────┴──────┴──────╯                                       ╰──────┴──────┴──────┴──────┴──────╯
  *                                        ╭──────┬──────╮       ╭──────┬────────╮
- *                                        │Home  │ End  │       │ CAPS │Ctrl/Esc│
+ *                                        │Home  │ End  │       │ CAPS │ RCtrl  │
  *                                 ╭──────┼──────┼──────┤       ├──────┼────────┼──────╮
  *                                 │      │      │ Apps │       │ PgUp │        │      │
- *                                 │ BkSp │ ~L1  ├──────┤       ├──────┤  ~L1   │ Space│
+ *                                 │ BkSp │  L2  ├──────┤       ├──────┤   L1   │ Space│
  *                                 │      │      │ PScr │       │ PgDn │        │      │
  *                                 ╰──────┴──────┴──────╯       ╰──────┴────────┴──────╯
  */
@@ -45,33 +51,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        CTL_T(KC_ESC),  KC_1,           KC_2,    KC_3,   KC_4,   KC_5,   KC_INS,
+        CTL_T(KC_ESC),  KC_1,           KC_2,    KC_3,   KC_4,   KC_5,   M(MACRO_ISEQUALS),
         KC_TAB,         KC_QUOT,        KC_COMM, KC_DOT, KC_P,   KC_Y,   KC_DELT,
-        KC_EQL,         LT(MDIA, KC_A), KC_O,    KC_E,   KC_U,   KC_I,
+        KC_EQL,         LT(ARWS, KC_A), KC_O,    KC_E,   KC_U,   KC_I,
         KC_LSFT,        CTL_T(KC_SCLN), KC_Q,    KC_J,   KC_K,   KC_X,   COMPI,
         KC_LCTRL, KC_LALT, CUT, COPY, PASTE,
                                               KC_HOME,  KC_END,
                                                  ALT_T(KC_APP),
-                                      KC_BSPC, KC_FN1, KC_PSCR,
+                                      KC_BSPC, KC_FN2, KC_PSCR,
         // right hand
-             KC_DELT,    KC_6,   KC_7,   KC_8,   KC_9,   KC_0,            KC_BSLS,
+             KC_INS,    KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_BSLS,
              KC_BSPC,   KC_F,   KC_G,   KC_C,   KC_R,   KC_L,             KC_SLSH,
-                        KC_D,   KC_H,   KC_T,   KC_N,   LT(MDIA, KC_S),   KC_MINS,
+                        KC_D,   KC_H,   KC_T,   KC_N,   LT(FKEY, KC_S),   KC_MINS,
              KC_ENT,    KC_B,   KC_M,   KC_W,   KC_V,   CTL_T(KC_Z),      KC_RSFT,
                                   KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT, KC_SLCK,
-             KC_CAPS, CTL_T(KC_ESC),
+             KC_CAPS, KC_RCTRL,
              KC_PGUP,
              KC_PGDN, KC_FN1, KC_SPC
 ),
-/* Keymap 1: Symbol Layer
+/* Keymap 1: Symbol Layer (red)
  *
  * ╭────────┬──────┬──────┬──────┬──────┬──────┬──────╮           ╭──────┬──────┬──────┬──────┬──────┬──────┬────────╮
- * │        │  F1  │  F2  │  F3  │  F4  │  F5  │      │           │      │  F6  │  F7  │  F8  │  F9  │ F10  │  F11   │
+ * │        │      │      │      │      │      │      │           │      │      │      │      │      │      │        │
  * ├────────┼──────┼──────┼──────┼──────┼──────┼──────┤           ├──────┼──────┼──────┼──────┼──────┼──────┼────────┤
- * │        │  !   │  @   │  {   │  }   │  |   │      │           │      │      │  7   │  8   │  9   │  *   │  F12   │
+ * │        │  !   │  @   │  {   │  }   │  |   │ TODO │           │      │      │  7   │  8   │  9   │  *   │        │
  * ├────────┼──────┼──────┼──────┼──────┼──────┤      │           │      ├──────┼──────┼──────┼──────┼──────┼────────┤
- * │        │  #   │  $   │  (   │  )   │  `   ├──────┤           ├──────┤      │  4   │  5   │  6   │  +   │        │
- * ├────────┼──────┼──────┼──────┼──────┼──────┤      │           │      ├──────┼──────┼──────┼──────┼──────┼────────┤
+ * │        │  #   │  $   │  (   │  ) • │  `   ├──────┤           ├──────┤      │• 4   │  5   │  6   │  +   │        │
+ * ├────────┼──────┼──────┼──────┼──────┼──────┤  ( ) │           │      ├──────┼──────┼──────┼──────┼──────┼────────┤
  * │        │      │  ^   │  [   │  ]   │  ~   │      │           │      │      │  1   │  2   │  3   │  \   │        │
  * ╰─┬──────┼──────┼──────┼──────┼──────┼──────┴──────╯           ╰──────┴──────┼──────┼──────┼──────┼──────┼──────┬─╯
  *   │      │      │      │      │      │                                       │  0   │  00  │  .   │  =   │      │
@@ -87,64 +93,103 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // SYMBOLS
 [SYMB] = KEYMAP(
        // left hand
-       KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-       KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
+       KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,M(MACRO_TODO),
        KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-       KC_TRNS,KC_TRNS,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
+       KC_TRNS,KC_TRNS,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,M(MACRO_PARENTHESE),
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                                        KC_TRNS,KC_TRNS,
                                                KC_TRNS,
                                KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
-       KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_TRNS, KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_7,   KC_8,    KC_9,    KC_ASTR, KC_TRNS,
                 KC_TRNS, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-                         KC_0,   KC_0,    KC_DOT,  KC_EQL,  KC_TRNS,
+                         KC_0,   M(MDBL0),KC_DOT,  KC_EQL,  KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
 ),
-/* Keymap 2: Media and mouse keys
+/* Keymap 2: Function keys (green)
  *
  * ╭────────┬──────┬──────┬──────┬──────┬──────┬──────╮           ╭──────┬──────┬──────┬──────┬──────┬──────┬────────╮
- * │ Teensy │      │      │      │      │      │      │           │      │      │      │      │      │      │        │
+ * │        │      │      │      │      │      │      │           │      │      │      │      │      │      │        │
  * ├────────┼──────┼──────┼──────┼──────┼──────┼──────┤           ├──────┼──────┼──────┼──────┼──────┼──────┼────────┤
- * │        │      │      │ MsUp │      │      │      │           │      │      │      │ Home │ PgUp │      │        │
+ * │        │  F1  │  F2  │  F3  │  F4  │  F5  │      │           │      │      │      │      │      │      │        │
  * ├────────┼──────┼──────┼──────┼──────┼──────┤      │           │      ├──────┼──────┼──────┼──────┼──────┼────────┤
- * │        │      │MsLeft│MsDown│MsRght│      ├──────┤           ├──────┤      │      │ End  │ PgDn │      │        │
+ * │        │  F6  │  F7  │  F8  │  F9 •│ F10  ├──────┤           ├──────┤      │•Shift│ Alt  │      │      │        │
  * ├────────┼──────┼──────┼──────┼──────┼──────┤      │           │      ├──────┼──────┼──────┼──────┼──────┼────────┤
- * │        │      │      │      │      │      │      │           │      │      │ Home │  Up  │ End  │      │        │
+ * │        │ F11  │ F12  │      │      │      │      │           │      │      │      │      │      │      │        │
  * ╰─┬──────┼──────┼──────┼──────┼──────┼──────┴──────╯           ╰──────┴──────┼──────┼──────┼──────┼──────┼──────┬─╯
- *   │      │      │      │      │      │                                       │ Left │ Down │ Right│      │      │
+ *   │      │      │      │      │      │                                       │      │      │      │      │      │
  *   ╰──────┴──────┴──────┴──────┴──────╯                                       ╰──────┴──────┴──────┴──────┴──────╯
  *                                        ╭──────┬──────╮       ╭──────┬────────╮
  *                                        │      │      │       │      │        │
  *                                 ╭──────┼──────┼──────┤       ├──────┼────────┼──────╮
- *                                 │      │      │      │       │      │        │Brwser│
- *                                 │ LClk │ RClk ├──────┤       ├──────┤        │ Back │
+ *                                 │      │      │      │       │      │        │      │
+ *                                 │LCtrl │      ├──────┤       ├──────┤        │ RCtrl│
  *                                 │      │      │      │       │      │        │      │
  *                                 ╰──────┴──────┴──────╯       ╰──────┴────────┴──────╯
  */
-// MEDIA AND MOUSE
-KEYMAP(
-       RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
+[FKEY] = KEYMAP(
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
+       KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+       KC_TRNS, KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS  , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                            KC_TRNS, KC_TRNS,
                                                     KC_TRNS,
-                                  KC_BTN1, KC_BTN2, KC_TRNS,
+                                  KC_LCTRL, KC_TRNS, KC_TRNS,
     // right hand
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_PSCR,  KC_TRNS, KC_TRNS, KC_HOME, KC_PGUP, KC_TRNS, KC_TRNS,
-                 KC_TRNS, KC_TRNS, KC_END,  KC_PGDN, KC_TRNS, KC_TRNS,
-       KC_TRNS,  KC_TRNS, KC_HOME, KC_UP  , KC_END,  KC_TRNS, KC_TRNS,
-                          KC_LEFT, KC_DOWN, KC_RIGHT,KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_TRNS, KC_RSFT, KC_RALT, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_WBAK
+       KC_TRNS, KC_TRNS, KC_RCTRL
+),
+/* Keymap 3: Arrow keys (blue)
+ *
+ * ╭────────┬──────┬──────┬──────┬──────┬──────┬──────╮           ╭──────┬──────┬──────┬──────┬──────┬──────┬────────╮
+ * │        │      │      │      │      │      │      │           │      │      │      │      │      │      │        │
+ * ├────────┼──────┼──────┼──────┼──────┼──────┼──────┤           ├──────┼──────┼──────┼──────┼──────┼──────┼────────┤
+ * │        │      │      │      │      │      │      │           │      │      │      │      │      │      │        │
+ * ├────────┼──────┼──────┼──────┼──────┼──────┤      │           │      ├──────┼──────┼──────┼──────┼──────┼────────┤
+ * │        │      │      │  Alt │Shift•│      ├──────┤           ├──────┤      │•     │      │      │      │        │
+ * ├────────┼──────┼──────┼──────┼──────┼──────┤      │           │      ├──────┼──────┼──────┼──────┼──────┼────────┤
+ * │        │      │      │      │      │      │      │           │      │      │ Home │  Up  │ End  │ PgUp │        │
+ * ╰─┬──────┼──────┼──────┼──────┼──────┼──────┴──────╯           ╰──────┴──────┼──────┼──────┼──────┼──────┼──────┬─╯
+ *   │      │      │      │      │      │                                       │ Left │ Down │ Right│ PgDn │      │
+ *   ╰──────┴──────┴──────┴──────┴──────╯                                       ╰──────┴──────┴──────┴──────┴──────╯
+ *                                        ╭──────┬──────╮       ╭──────┬────────╮
+ *                                        │      │      │       │      │        │
+ *                                 ╭──────┼──────┼──────┤       ├──────┼────────┼──────╮
+ *                                 │      │      │      │       │      │        │      │
+ *                                 │LCtrl │      ├──────┤       ├──────┤        │ RCtrl│
+ *                                 │      │      │      │       │      │        │      │
+ *                                 ╰──────┴──────┴──────╯       ╰──────┴────────┴──────╯
+ */
+[ARWS] = KEYMAP(
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_LALT, KC_LSFT, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                           KC_TRNS, KC_TRNS,
+                                                    KC_TRNS,
+                                  KC_LCTRL, KC_TRNS, KC_TRNS,
+    // right hand
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_HOME, KC_UP  , KC_END,  KC_PGUP, KC_TRNS,
+                          KC_LEFT, KC_DOWN, KC_RIGHT,KC_PGDN, KC_TRNS,
+       KC_TRNS, KC_TRNS,
+       KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_RCTRL
 ),
 };
 
@@ -171,22 +216,47 @@ KEYMAP(
  */
 
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
+    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB),                // FN1 - Momentary Layer 1 (Symbols)
+    [2] = ACTION_LAYER_TAP_TOGGLE(FKEY),                // FN2 - Momentary Layer 2 (F-keys)
+    [3] = ACTION_LAYER_TAP_TOGGLE(ARWS)                 // FN3 - Momentary Layer 3 (Arrows)
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
   // MACRODOWN only works in this function
       switch(id) {
-        case 0:
-        if (record->event.pressed) {
-          register_code(KC_RSFT);
-        } else {
-          unregister_code(KC_RSFT);
-        }
-        break;
+		case MACRO_TODO:
+			if (record->event.pressed) {
+				return MACRO( T(SLASH), T(SLASH), D(LSHIFT), T(T), T(O), T(D), T(O), U(LSHIFT), T(SPACE), END );
+			} 
+			break;
+		case MACRO_ISEQUALS:
+			if (record->event.pressed) {
+				return MACRO( D(LSFT), T(SCOLON), U(LSHIFT), T(EQL), T(SPC), END );
+			} 
+			break;
+		case MACRO_PARENTHESE:
+			if (record->event.pressed) {
+				return MACRO(  D(LSHIFT),T(9), T(0),U(LSHIFT), T(LEFT), END);
+			} 
+			break;
+		case MDBL0:
+			if (record->event.pressed) {
+				return MACRO( T(0), T(0), END );
+			}
+			break;
       }
     return MACRO_NONE;
+
+//      case 0:
+//      if (record->event.pressed) {
+//        register_code(KC_RSFT);
+//      } else {
+//        unregister_code(KC_RSFT);
+//      }
+//      break;
+//    }
+//  return MACRO_NONE;
 };
 
 // Runs just one time when the keyboard initializes.
@@ -205,11 +275,14 @@ void matrix_scan_user(void) {
     ergodox_right_led_3_off();
     switch (layer) {
       // TODO: Make this relevant to the ErgoDox EZ.
-        case 1:
+        case SYMB:
             ergodox_right_led_1_on();
             break;
-        case 2:
+        case FKEY:
             ergodox_right_led_2_on();
+            break;
+        case ARWS:
+            ergodox_right_led_3_on();
             break;
         default:
             // none
