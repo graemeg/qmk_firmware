ae2dff31dce9bbc045a7e67adcad667d57d31aea
Graeme Geldenhuys <graemeg@gmail.com>
2017-05-04 17:06:51 +0100

    Add QWERTY layer, removes timer code, reduce LED brightness, adds VER and RESET buttons.

--- keyboards/ergodox/keymaps/gg_dvorak/keymap.c
+++ keyboards/ergodox/keymaps/gg_dvorak/keymap.c
@@ -3,19 +3,19 @@
 #include "action_layer.h"
 /* #include "keymap_extras/keymap_uk.h" */
 #include "process_unicode.h"
-#include "timer.h"
+#include "version.h"
 
 #define PREVENT_STUCK_MODIFIERS
 
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
 #define FKEY 2 // function keys
-#define NPAD 3 // number pad
+#define QWER 3 // QWERTY
 
 
 /*
   Author: Graeme Geldenhuys <graeme@geldenhuys.co.uk>
-  Ver 1.7
+  Ver 1.11
   Based on the default Dvorak keymap (2016-08-16), and applied some
   tweaks from the TypeMatrix 2030 design. Some key locations are also
   designed to work well with my programming in Lazarus and Delphi.
@@ -29,30 +29,16 @@
 /* Custom keycodes */
 
 enum {
-  NONE = 0,
+  NONE = 0, // SAFE_RANGE, // can always be here
 
   // useful shortcuts
   MACRO_TODO,
   MACRO_ISEQUALS,
   MACRO_PARENTHESE,
   MDBL0,
-
-  // Function / number keys
-  KF_1, // 1, F1
-  KF_2, // 2, F2
-  KF_3, // ...
-  KF_4,
-  KF_5,
-  KF_6,
-  KF_7,
-  KF_8,
-  KF_9,
-  KF_10,
-  KF_11,
-  KF_12
+  EPRM,
+  VRSN
 };
-
-uint16_t kf_timers[12];
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -102,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 1: Symbol Layer (red)
  *
  * ╭────────┬──────┬──────┬──────┬──────┬──────┬──────╮           ╭──────┬──────┬──────┬──────┬──────┬──────┬────────╮
- * │        │      │      │      │      │      │      │           │      │      │      │      │      │      │        │
+ * │        │      │      │      │      │      │      │           │ QWER │      │      │      │      │      │        │
  * ├────────┼──────┼──────┼──────┼──────┼──────┼──────┤           ├──────┼──────┼──────┼──────┼──────┼──────┼────────┤
  * │   |    │  !   │  @   │  {   │  }   │  +   │ TODO │           │      │      │  7   │  8   │  9   │  *   │        │
  * ├────────┼──────┼──────┼──────┼──────┼──────┤      │     1     │      ├──────┼──────┼──────┼──────┼──────┼────────┤
@@ -132,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                  KC_TRNS,
                               KC_TRNS, KC_TRNS,  KC_TRNS,
        // right hand
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_FN3,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_7,   KC_8,    KC_9,    KC_ASTR, KC_TRNS,
                 KC_TRNS, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
@@ -180,7 +166,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
-)
+),
+/* Keymap 1: QWERTY layer (games)
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |        |      |      |      |      |      |  L0  |           |      |      |      |      |      |      |        |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |        |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |      |      |      |        |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |      |      |      |      |      |                                       |      |      |      |      | RESET|
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      |      |
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 |      |      |      |       |      |        |      |
+ *                                 |      |      |------|       |------|        |      |
+ *                                 |      |      | VER  |       |      |        |      |
+ *                                 `--------------------'       `----------------------'
+ */
+[QWER] = KEYMAP(  // layer 3: QWERTY layer (games) 
+       KC_TRNS,        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_FN3,
+       KC_TRNS,        KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_TRNS,
+       KC_TRNS,        KC_A,       KC_S,       KC_D,       KC_F,       KC_G,
+       KC_TRNS,        KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_TRNS,
+       KC_TRNS,        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+
+                       KC_TRNS,    KC_TRNS,
+                                   KC_TRNS,
+       KC_TRNS,        KC_TRNS,    M(VRSN),
+
+    // right hand
+       KC_TRNS,        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+       KC_TRNS,        KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_TRNS,
+                       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_TRNS,
+       KC_TRNS,        KC_N,       KC_M,       KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+                                   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    RESET,
+       KC_TRNS,        KC_TRNS,
+       KC_TRNS,
+       KC_TRNS,        KC_TRNS,    KC_TRNS
+    )
 };
 
 /* Empty template layer - copy and paste usage
@@ -208,32 +236,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM fn_actions[] = {
     [1] = ACTION_LAYER_TAP_TOGGLE(SYMB),                // FN1 - Momentary Layer 1 (Symbols)
     [2] = ACTION_LAYER_TAP_TOGGLE(FKEY),                // FN2 - Momentary Layer 2 (F-keys)
-    [3] = ACTION_LAYER_TAP_TOGGLE(NPAD)                 // FN3 - Momentary Layer 3 (Numpad)
+    [3] = ACTION_LAYER_TAP_TOGGLE(QWER)                 // FN3 - Momentary Layer 3 (Numpad)
 };
 
-void ang_handle_kf (keyrecord_t *record, uint8_t id)
-{
-  uint8_t code = id - KF_1;
-
-  if (record->event.pressed) {
-    kf_timers[code] = timer_read ();
-  } else {
-    uint8_t kc;
-
-    if (timer_elapsed (kf_timers[code]) > TAPPING_TERM) {
-      // Long press
-      kc = KC_F1 + code;
-    } else {
-      if (id == KF_11)
-        kc = KC_EQL;
-      else
-        kc = KC_1 + code;
-    }
-
-    register_code (kc);
-    unregister_code (kc);
-  }
-}
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
@@ -259,9 +264,15 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 				return MACRO( T(0), T(0), END );
 			}
 			break;
-		/* Function keys */
-		case KF_1 ... KF_10:
-			ang_handle_kf (record, id);
+		case EPRM:
+			if (record->event.pressed) { // For resetting EEPROM
+				eeconfig_init();
+			}
+			break;
+		case VRSN:
+			if (record->event.pressed) {
+				SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+			}
 			break;
       }
     return MACRO_NONE;
@@ -290,12 +301,18 @@ void matrix_init_user(void) {
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
 
+    static uint8_t state;
     uint8_t layer = biton32(layer_state);
 
     ergodox_board_led_off();
     ergodox_right_led_1_off();
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
+
+    //reduce LED on time to 1/6th because LEDs are too strong
+    if (++state < 6) return;
+    state = 0;
+
     switch (layer) {
       // TODO: Make this relevant to the ErgoDox EZ.
         case SYMB:
@@ -304,7 +321,7 @@ void matrix_scan_user(void) {
         case FKEY:
             ergodox_right_led_2_on();
             break;
-        case NPAD:
+        case QWER:
             ergodox_right_led_3_on();
             break;
         default:
